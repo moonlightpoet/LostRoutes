accf9f4a2e3a812a7dfc42ac9b57ab286df46943
moonlightpoet <moonlightpoet@163.com>
2016-07-28 14:45:29 +0800

    add SettingScene

--- src/HomeScene.js
+++ src/HomeScene.js
@@ -28,7 +28,7 @@ var HomeMenuLayer = cc.Layer.extend({
         end.x = winSize.width / 2;
         end.y = end.getContentSize().height / 2;
         this.addChild(end);
-    }
+
         /*
         // 开始菜单
         var startSpriteNormal = new cc.Sprite("#button.start.png");
@@ -38,7 +38,7 @@ var HomeMenuLayer = cc.Layer.extend({
             startSpriteSelected,
             this.menuItemCallback, this);
         startMenuItem.setTag(HomeMenuActionTypes.MenuItemStart);
-
+*/
         // 设置菜单
         var settingSpriteNormal = new cc.Sprite("#button.setting.png");
         var settingSpriteSelected = new cc.Sprite("#button.setting-on.png");
@@ -47,7 +47,7 @@ var HomeMenuLayer = cc.Layer.extend({
             settingSpriteSelected,
             this.menuItemCallback, this);
         settingMenuItem.setTag(HomeMenuActionTypes.MenuItemSetting);
-
+/*
         // 帮助菜单
         var helppriteNormal = new cc.Sprite("#button.help.png");
         var helpSpriteSelected = new cc.Sprite("#button.help-on.png");
@@ -56,8 +56,9 @@ var HomeMenuLayer = cc.Layer.extend({
             helpSpriteSelected,
             this.menuItemCallback, this);
         helpMenuItem.setTag(HomeMenuActionTypes.MenuItemHelp);
-
-        var mu = new cc.Menu(startMenuItem, settingMenuItem, helpMenuItem);
+*/
+        //var mu = new cc.Menu(startMenuItem, settingMenuItem, helpMenuItem);
+        var mu = new cc.Menu(settingMenuItem);
         mu.x = winSize.width / 2;
         mu.y = winSize.height / 2;
         mu.alignItemsVerticallyWithPadding(10);
@@ -65,7 +66,6 @@ var HomeMenuLayer = cc.Layer.extend({
         this.addChild(mu);
         return true;
     },
-
     menuItemCallback: function (sender) {
         //播放音效
         if (effectStatus == BOOL.YES) {
@@ -106,7 +106,6 @@ var HomeMenuLayer = cc.Layer.extend({
         cc.log("HomeMenuLayer onExitTransitionDidStart");
         cc.audioEngine.stopMusic(res_platform.musicHome);
     }
-    */
 });
 
 var HomeScene = cc.Scene.extend({
